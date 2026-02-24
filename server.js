8e2ba74091c9a7e954360f89015b52d8029ec43e
aliakbarr474 <aliakbarr474@gmail.com>
2026-02-25 00:46:23 +0500

    fix: update catch-all route syntax for Express 5

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html')); 
 });
 
 app.use(express.json());
