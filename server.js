54cba3fbb69784bb5d5ce94c0746f6d567cdb60f
aliakbarr474 <aliakbarr474@gmail.com>
2026-02-26 13:07:06 +0500

    pool fix

--- server.js
+++ server.js
@@ -17,12 +17,18 @@ app.get('/{*splat}', (req, res) => {
 
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "1234",
   database: process.env.DB_NAME || "project",
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 db.connect((err) => {
